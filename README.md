7178b24c1c34a09efa427c8161e721e5cdb7fc95
MrJonRoberts <jroberts@tas.qld.edu.au>
2021-03-18 14:28:10 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# initial_flask
+Initial flask repo - scripts, css, fav icon.  Base Template and homepage
---
+++ app.py
@@ -0,0 +1,17 @@
+
+from flask import Flask, render_template
+# test for xg
+# test 2
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def home():
+    title = "Home Page"
+    pageHeader = "This is the home page it is cool!"
+    return render_template("home.html", title=title, pageHeader=pageHeader)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=8080)
