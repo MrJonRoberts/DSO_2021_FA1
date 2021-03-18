89fc1fd5e677e505fa589e477c8a675cdc28fb40
MrJonRoberts <jroberts@tas.qld.edu.au>
2021-03-18 14:29:09 +1000

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,5 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    # app.run(debug=True, host="0.0.0.0", port=8080)   <- run on external ip
+    app.run(debug=True, port=5000)
